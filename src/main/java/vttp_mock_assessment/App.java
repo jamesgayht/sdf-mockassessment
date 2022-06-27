7ac32157fee3eff002d0842877224a5b48593e3d
James Gay <jumo@Jamess-MacBook-Air.local>
2022-06-27 15:24:53 +0800

    first commit

---
+++ src/main/java/vttp_mock_assessment/App.java
@@ -0,0 +1,13 @@
+package vttp_mock_assessment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
